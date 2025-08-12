931c938e259611340dca9947616be0f9eb0217cb
Dennis wambua <muasyathegreat4@gmail.com>
2025-08-12 09:30:34 +0300

    Simplified CORS config to use CORS_ALLOW_ALL_HEADERS = True without conflicting specific headers list

--- winas_sacco/settings.py
+++ winas_sacco/settings.py
@@ -102,17 +102,13 @@ CORS_ALLOWED_ORIGINS=[
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_HEADERS = True
-CORS_ALLOWED_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'x-dev-request-id',
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
