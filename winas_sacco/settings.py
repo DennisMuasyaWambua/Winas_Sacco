cd0a7e932d3750d02519975fd0b54b3c494712cb
Dennis wambua <muasyathegreat4@gmail.com>
2025-08-19 17:20:41 +0300

    http://localhost:5173/  added to allowed origins

--- winas_sacco/settings.py
+++ winas_sacco/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xmg9&$y1gv2u80(1*c(a!q)4vsjfdg=k2%iax_3f1#rw7el27_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['winassacco-production.up.railway.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['winassacco-production.up.railway.app', '127.0.0.1', 'localhost', 'http://localhost:5173']
 
 
 # Application definition
